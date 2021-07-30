0747617aeed5072fb478caffab857c6dbd3d6a61
drifterz13 <mixmyxxe@gmail.com>
2021-07-30 16:37:33 +0700

    use prompt-ui

--- cmd/report.go
+++ cmd/report.go
@@ -17,10 +17,12 @@ package cmd
 
 import (
 	"fmt"
+	"log"
 	"os"
 	"strconv"
 
 	"github.com/drifterz13/tw-reporter-cli/api"
+	"github.com/manifoldco/promptui"
 	"github.com/olekukonko/tablewriter"
 	"github.com/spf13/cobra"
 )
@@ -38,54 +40,108 @@ var reportCmd = &cobra.Command{
 	Short: "Report project status",
 	Long:  `Report project status`,
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("getting tasklists...")
-		tasklists := api.GetTasklists()
+		// report()
+		reportBySelectTasklist()
+	},
+}
 
-		var tableData [][]string
+func report() {
+	fmt.Println("getting tasklists...")
+	tasklists := api.GetTasklists()
 
-		usersChan := make(chan []api.User, 1)
+	var tableData [][]string
 
-		fmt.Println("getting users...")
-		go api.GetWorkspaceUsers(usersChan)
+	usersChan := make(chan []api.User, 1)
 
-		users := <-usersChan
+	fmt.Println("getting users...")
+	go api.GetWorkspaceUsers(usersChan)
 
-		taskReportRow := map[string]TaskReportRow{}
-		var allTasks []api.Task
+	users := <-usersChan
 
-		fmt.Println("getting tasks...")
-		for _, tasklist := range tasklists {
-			tasks := api.GetTasks(&tasklist)
-			for _, task := range tasks {
-				allTasks = append(allTasks, task)
-				taskReportRow[task.ID] = TaskReportRow{
-					TasklistTitle: tasklist.Title,
-					TaskTitle:     task.Title,
-					TaskPoints:    task.Points,
-				}
+	taskReportRow := map[string]TaskReportRow{}
+	var allTasks []api.Task
+
+	fmt.Println("getting tasks...")
+	for _, tasklist := range tasklists {
+		tasks := api.GetTasks(&tasklist)
+		for _, task := range tasks {
+			allTasks = append(allTasks, task)
+			taskReportRow[task.ID] = TaskReportRow{
+				TasklistTitle: tasklist.Title,
+				TaskTitle:     task.Title,
+				TaskPoints:    task.Points,
 			}
 		}
+	}
 
-		for _, task := range allTasks {
-			assignees := api.GetTaskAssignees(task, users)
-			row := taskReportRow[task.ID]
-			row.TaskAssignees = assignees
-			taskReportRow[task.ID] = row
-		}
+	for _, task := range allTasks {
+		assignees := api.GetTaskAssignees(task, users)
+		row := taskReportRow[task.ID]
+		row.TaskAssignees = assignees
+		taskReportRow[task.ID] = row
+	}
 
-		for _, v := range taskReportRow {
-			row := append([]string{}, v.TasklistTitle, v.TaskTitle, strconv.Itoa(v.TaskPoints), v.TaskAssignees)
-			tableData = append(tableData, row)
-		}
+	for _, v := range taskReportRow {
+		var row []string
+		row = append(row, v.TasklistTitle, v.TaskTitle, strconv.Itoa(v.TaskPoints), v.TaskAssignees)
+		tableData = append(tableData, row)
+	}
 
-		table := tablewriter.NewWriter(os.Stdout)
-		table.SetHeader([]string{"Tasklist", "Task", "Points", "Assignees"})
-		table.SetRowLine(true)
-		table.AppendBulk(tableData)
-		table.Render()
-	},
+	table := tablewriter.NewWriter(os.Stdout)
+	table.SetHeader([]string{"Tasklist", "Task", "Points", "Assignees"})
+	table.SetRowLine(true)
+	table.AppendBulk(tableData)
+	table.Render()
+}
+
+func reportBySelectTasklist() {
+	usersChan := make(chan []api.User, 1)
+	go api.GetWorkspaceUsers(usersChan)
+
+	fmt.Println("getting tasklists...")
+	tasklists := api.GetTasklists()
+
+	var items []string
+
+	for _, tasklist := range tasklists {
+		items = append(items, tasklist.Title)
+	}
+
+	prompt := promptui.Select{
+		Label: "Select tasklist",
+		Items: items,
+	}
+
+	index, result, err := prompt.Run()
+	if err != nil {
+		log.Fatalf("prommpt failed %v\n", err)
+	}
+
+	fmt.Printf("select: %v, index: %v\n", result, index)
+
+	var tableData [][]string
+
+	tasklist := &tasklists[index]
+	tasks := api.GetTasks(tasklist)
+	users := <-usersChan
+
+	for _, task := range tasks {
+		var row []string
+
+		assignees := api.GetTaskAssignees(task, users)
+		row = append(row, tasklist.Title, task.Title, strconv.Itoa(task.Points), assignees)
+		tableData = append(tableData, row)
+	}
+
+	table := tablewriter.NewWriter(os.Stdout)
+	table.SetHeader([]string{"Tasklist", "Task", "Points", "Assignees"})
+	table.SetRowLine(true)
+	table.AppendBulk(tableData)
+	table.Render()
+
 }
 
 func init() {
 	rootCmd.AddCommand(reportCmd)
+
 }
--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module github.com/drifterz13/tw-reporter-cli
 go 1.16
 
 require (
+	github.com/manifoldco/promptui v0.8.0 // indirect
 	github.com/olekukonko/tablewriter v0.0.5 // indirect
 	github.com/spf13/cobra v1.2.1
 	github.com/spf13/viper v1.8.1
